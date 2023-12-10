3e3e3add8b6b9af1d6d39b59fb3fdcd615d56566
Md Rakib Hossen <rakib.dev70@gmail.com>
2023-12-11 00:37:59 +0600

    136

--- Codeforces/136_A.cpp
+++ Codeforces/136_A.cpp
@@ -6,24 +6,19 @@ int main()
 {
     int n;
     cin >> n;
-
     int arr1[n], arr2[n];
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> arr1[i];
     }
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        arr2[arr1[i] - 1] = i + 1;
+        arr2[arr1[i]] = i;
     }
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cout << arr2[i] << " ";
     }
-
     cout << endl;
     return 0;
-}
+};
