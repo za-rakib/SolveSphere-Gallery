439e2ab3d3c796f84b4670a7523b143bc1a49c9e
Rakib <rakib@sayburgh.com>
2024-09-01 15:23:00 +0600

    code force 1742 A

---
+++ Codeforces/1742_A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c,t;
+    cin>>t;
+    while (t--)
+    {
+       cin >> a >> b >> c;
+
+    if (a + b == c || a + c == b || b + c == a) {
+        cout << "Yes"<<endl;
+    } else {
+        cout << "No"<<endl;
+    }
+    }
+    
+    return 0;
+}
