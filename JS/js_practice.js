7db942eb30b8d5fbc70b0cfb1adaa5dc9821ee1b
Rakib <rakib@sayburgh.com>
2024-11-28 18:57:12 +0600

    js hoisting

--- JS/js_practice.js
+++ JS/js_practice.js
@@ -3,11 +3,24 @@
 // Variables: Hoisted but initialized as undefined.
 // let/const: Hoisted but not initialized (temporal dead zone). and get get an initialisation error.
 
-function showData() {
-  console.log("Name:", name);
-  console.log("Age:", age);
-  var name = "Rakib";
-  let age = 27;
-}
-showData(); 
+// 1
+// function showData() {
+//   console.log("Name:", name);
+//   console.log("Age:", age);
+//   var name = "Rakib";
+//   let age = 27;
+// }
+// showData(); 
+
 // output 
+
+// 2
+for(var i =0 ; i<4; i++){
+  setTimeout(()=>console.log(i))
+}
+
+for(let i =0 ; i<4; i++){
+  setTimeout(()=>console.log(i))
+}
+
+// output
